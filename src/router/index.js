bd6c58a30cc6b8ba6c0148e737b3ce9336b99cf8
git <admin@lianyue.org>
2017-09-19 11:58:39 +0900

    Add 'vue-upload-component'

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ Vue.use(Router)
 const createListView = id => () => import('../views/CreateListView').then(m => m.default(id))
 const ItemView = () => import('../views/ItemView.vue')
 const UserView = () => import('../views/UserView.vue')
+const UploadView = () => import('../views/UploadView.vue')
 
 export function createRouter () {
   return new Router({
@@ -20,6 +21,7 @@ export function createRouter () {
       { path: '/job/:page(\\d+)?', component: createListView('job') },
       { path: '/item/:id(\\d+)', component: ItemView },
       { path: '/user/:id', component: UserView },
+      { path: '/upload', component: UploadView },
       { path: '/', redirect: '/top' }
     ]
   })
---
+++ src/views/UploadView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="upload-view">
+    <ul>
+      <li v-for="file in files">
+        {{file.name}}
+      </li>
+    </ul>
+    <file-upload
+      post-action="/post"
+      :multiple="true"
+      :thread="3"
+      :drop="true"
+      :drop-directory="true"
+      v-model="files"
+      ref="upload">
+      Add upload files
+    </file-upload>
+  </div>
+</template>
+
+<script>
+import FileUpload from 'vue-upload-component/src'
+export default {
+  components: { FileUpload },
+
+  data: () => ({
+    files: [],
+  }),
+
+
+  title () {
+    return 'Upload'
+  },
+}
+</script>
